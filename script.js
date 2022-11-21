6ea2f6d22f35e5be431579840e01d536c9fdfa95
berker35 <berker35@gmail.com>
2022-11-21 18:25:45 +0300

    Update script.js
    Added some comments.

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+//Query selecting made easier.
 const $ = (selector) => document.querySelector(selector);
+// When city name entered, submit handler function trims the city name
+// and invokes the main function with it.
 $("form").addEventListener("submit", (e) => {
   e.preventDefault();
   let cityName = e.target[0].value;
   bringInfo(cityName.trim());
 });
+//Main function
 const bringInfo = async (cityName) => {
+  // cityName parameter goes to api url dinamically.
   const api = `https://api.openweathermap.org/data/2.5/weather?appid=1dfc18cb9aee578073a925c7b383a8db&q=${cityName}&units=metric`;
   const response = await fetch(api);
   const data = await response.json();
+  // Below, the function fills the app with
+  // weather data coming from the api.
   const imgUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
   $(".city-name").innerText = `${data.name}`;
   $(".weather-description").innerText = `${data.weather[0].description}`;
@@ -19,4 +26,5 @@ const bringInfo = async (cityName) => {
   $(".humidity").innerText = `${data.main.humidity}%`;
   $(".pressure").innerText = `${data.main.pressure + "mb"}`;
 };
+// Just some placeholder city
 bringInfo("Tokyo");
